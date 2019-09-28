81df0ee409b53fa456bd86e773455e1f012ac1bf
olegpro <olegpro@yandex-team.ru>
2019-09-29 01:16:38 +0300

    bot view

--- src/api/chat/views.py
+++ src/api/chat/views.py
@@ -1,8 +1,51 @@
 import json
+import os
+import pandas as pd
 from django.http import JsonResponse
+from deeppavlov.contrib.skills.similarity_matching_skill import SimilarityMatchingSkill
 
 from api.chat.models import Message
 from api.auth.views import AuthenticatedView
+from api.chat.models import Faq
+
+FAQ_PATH = '/tmp/faq_psycho.csv'
+
+
+class ChatBotPredictor:
+    def __init__(self):
+        self._clf = None
+
+    def load_clf(self):
+        rows = Faq.objects.filter(published=True).values_list('question',
+                                                              'answer')
+        df = pd.DataFrame(rows, columns=['Question', 'Answer'])
+        df.to_csv(FAQ_PATH, index=False)
+
+        self._clf = SimilarityMatchingSkill(
+            data_path=FAQ_PATH,
+            x_col_name='Question',
+            y_col_name='Answer',
+            save_load_path='./model',
+            config_type='tfidf_autofaq',
+            edit_dict={},
+            train=True
+        )
+
+    def predict(self, message, threshold=0.7):
+        if self._clf is None:
+            self.load_clf()
+
+        try:
+            predictions = self._clf([message], [], [])
+            for text, prob in zip(*predictions):
+                if prob >= threshold:
+                    return text
+        except:
+            return None
+        return None
+
+
+predictor = ChatBotPredictor()
 
 
 class PostMessageView(AuthenticatedView):
@@ -14,15 +57,15 @@ class PostMessageView(AuthenticatedView):
         m = Message(message=message, user_email=user.email, author_email=user.email)
         m.save()
 
-        automatic_response = True  # todo: tf/idf
+        automatic_response = predictor.predict(message)
         if automatic_response:
-            response = 'stub'
-            m = Message(message=response, user_email=user.email, author_email='solar_bot')
+
+            m = Message(message=automatic_response, user_email=user.email, author_email='solar_bot')
             m.save()
 
             return JsonResponse({
                 'bot': True,
-                'response': response,
+                'message': automatic_response,
             }, status=200)
         else:
             return JsonResponse({
